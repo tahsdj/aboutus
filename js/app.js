ade623f595a8ce0626def5f59a8d81f0b4b38876
tahsdj <tahsdj@gmail.com>
2017-04-13 11:29:24 +0800

    add click function

--- js/app.js
+++ js/app.js
@@ -4,14 +4,42 @@ $(function(){
 	    let speed = 50
 	    if( scrollVal >= 10 ) {
 	        $("header.top").css({
-	        		"background-color": "#46B7B9",
-	        		"opacity": "0.5"
-	        	})
+        		"background-color": "#46B7B9",
+        		"opacity": "0.5",
+        		"color": "#DFF5F2"
+        	})
+	        
 	    }else{
 	    	$("header.top").css({
-	        		"background-color": "white",
-	        		"opacity": "1"
-	        	})
+        		"background-color": "white",
+        		"opacity": "1",
+        		"color": "#2F9296"
+        	})
+
 	    }
+	})
+});
+
+$(function(){
+	$('.intros .member a').click(function(){
+		const index = $(this).parents('.member').index()
+		$(this).html('<br>')
+		switch(index){
+			case 0:
+				$(this).parents('.intro').append('<p>你知道的太多了</p>')
+				break
+			case 1:
+				$(this).parents('.intro').append('<p>沒有更多了</p>')
+				break
+			case 2:
+				$(this).parents('.intro').append('<p>同上</p>')
+				break
+			case 3:
+				$(this).parents('.intro').append('<a>更少..</a>')
+				break
+			case 4:
+				$(this).parents('.intro').append('<p>忘了寫ㄏㄏ</p>')
+				break
+		}
 	})
 })
